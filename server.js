caa1287c93cf8f0281b28a24865b408069041338
JACK YARBOROUGH <jcyarbor@us.ibm.com>
2018-11-15 17:04:24 -0600

    changed styles.css, updated h1,h2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express"),
     app = express();
+	
+const https = require("https");
 
 var port = process.env.PORT || 8080;
 
@@ -7,6 +9,22 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
+  https.post('https://findmyfbid.com/?__amp_source_origin=https://findmyfbid.com','url='+user_name, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+  }).on("error", (err) => {
+	console.log("Error: " + err.message);
+  });
   response.end("Hello " + user_name + "!");
 });
 
