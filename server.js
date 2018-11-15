8a785f3fac5780fa51cb4d43e31892cc35ba4a11
JACK YARBOROUGH <jcyarbor@us.ibm.com>
2018-11-15 17:34:39 -0600

    updated server.js to make callout to findmyfbid.com

--- server.js
+++ server.js
@@ -1,45 +1,16 @@
 var express = require("express"),
-    app = express();
-const querystring = require("querystring");	
-const https = require("https");
+app = express();
 
 var port = process.env.PORT || 8080;
+
+const querystring = require("querystring");
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
-  var url = request.query.user_name;
-  
-  var postData = querystring.stringify({'url' : url});
-  
-  var options = {
-	  hostname: 'findmyfbid.com',
-	  port: 443,
-	  path: '/',
-	  method: 'POST',
-	  headers: {
-		  'Content-Type': 'application/x-www-form-urlencoded',
-		  'Content-length': postData.length
-	  }
-  };
-  
-  var req = https.request(options, (res) => {
-	  console.log('statusCode: ', res.statusCode);
-	  console.log('headers: ', res.headers);
-	  
-	  res.on('data', (d) =>{
-	  user_name = JSON.parse(d).explanation;
-	  });
-  });
-  
-  req.on('error', (e) => {
-	  console.error(e);
-  });
-  
-  req.write(postData);
-  req.end();
+	var user_name = request.query.user_name;
 
-  response.end("Hello " + user_name + "!");
+	response.end("Hello " + user_name + "!");
 });
 
 app.listen(port);
