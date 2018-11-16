f7ec5f3f5498b59b725c808ab7be6b1d7ca255b3
davidshenusibm <45082475+davidshenusibm@users.noreply.github.com>
2018-11-16 09:55:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.get("/sayHello", function (request, response) {
 	response.end("Hello " + user_name + "!");
 });
 
-//curl -i -X GET “https://graph.facebook.com/v3.2/{user_id}/picture?access_token={access_token}”
 //profile url is https://www.facebook.com/profile.php?id=100014295046013
 //parse for profile id
 
@@ -42,9 +41,11 @@ app.get("/searchUser", function(request, response) {
 		
 	//return response;
 
-//profile id 100014295046013
-
-
+//profile id should be 100014295046013
+//Get the picture from facebook profile
+		
+//curl -i -X GET “https://graph.facebook.com/v3.2/{user_id}/picture?access_token={access_token}”
+		
 		//const options = {
 			//hostname: 'https://graph.facebook.com/v3.2/100014295046013/picture?access_token='
 			//port: 443,
@@ -56,10 +57,29 @@ app.get("/searchUser", function(request, response) {
 			}
 		//}
 
-	//response.send("Juan Julio is from Hacker Valley, WV, USA")
+		//sending photo to tineye
+		
+		//const options = {
+		//hostname: https://www.tineye.com/search
+		//port: 443.
+		//path: '/'
+		//methos: 'POST'
+		//headers: {
+		//accept: application/json
+		
+		//return 10 results;
+		
 
+		
+		//location information request.
+		
 //curl -i -X GET “https://graph.facebook.com/v3.2/100014295046013?fields=location&access_token=EAAImoSZC5N1MBACKUQLLzPtDOuyzcaG5peDLDPdTNk7lv9EcGG7w5N3f9FvtEO5yKQgzcHUQxBHMvKSDwEFxE00Ov0zbj6PEZB7yL84fgZCLZCqab4mZBZAnP8ZBKZCgvMDZCXkYxDxmJZB4Hl9O8VQvZBOntu0Y0jZB8yJ9pEhfbPWA4tUHSWWT10K6WNF5wX716PJvjGjlUlxOtw2S0GdGI9L43oBjcOSqrSwqSPU4OcNhMU87UZA30GKCS”
 
+		
+		
+		//hardcoded response
+		//response.send("Juan Julio is from Hacker Valley, WV, USA")
+		
 //curl -v -X -H “accept: application/json” -d “url=https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/46362759_496818504137980_2517528254877794304_n.jpg?_nc_cat=106&_nc_ht=scontent.xx&oh=5bcdfb420fea99e1c8ccfd205363cc15&oe=5C71166E
 	//}
 	}
